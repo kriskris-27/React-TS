6aa2521e76eb48c0051156eb196bb9972e62f2eb
kriskris <krishnakumars0101@gmail.com>
2025-06-11 19:01:19 +0530

    Userdetail with useEffect

--- src/components/UserDetailFetcher.tsx
+++ src/components/UserDetailFetcher.tsx
@@ -10,13 +10,15 @@ const UserDetailFetcher = () => {
     const [data,setData] = useState<Udetail|null>()
     const [loading,setLoading] = useState<boolean>(false)
     const [error,setError] = useState<string|null>(null)
+    const [userId,setUserId] = useState<number>(1)
 
     useEffect(()=>{
         const fetchdata=async()=>{
+            
             setLoading(true)
             setError(null)
             try{
-                const resp = await fetch("https://jsonplaceholder.typicode.com/users/1")
+                const resp = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
 
                 if(!resp.ok){
                     throw new Error(`HTTP error! status: ${resp.status}`)
@@ -40,10 +42,10 @@ const UserDetailFetcher = () => {
             
         }
         fetchdata();
-    },[])
+    },[userId])
   return (
     <>
-
+    <input type='number' value={userId} onChange={(e)=>setUserId(e.target.valueAsNumber || 1)}/>
     {loading && <p>Loading...</p>}
     {error && (<p>Error:{error}</p>)}
     {!loading && !error && data && (<p>Name: {data.name} Email: {data.email}</p>)}
